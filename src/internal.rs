701b8f209be5faff07dd0450fd7e9a928b574255
Antti Keränen <detegr@gmail.com>
2017-10-21 11:16:03 +0300

    Relicense under MIT/Apache-2.0

--- src/internal.rs
+++ src/internal.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2017 fd developers
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0>
+// or the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>,
+// at your option. All files in the project carrying such
+// notice may not be copied, modified, or distributed except
+// according to those terms.
+
 use std::process;
 use std::time;
 use std::io::Write;
