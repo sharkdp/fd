2bc72a5eca6e3b6c8bdd2174bf4fa0aeecf7314d
Thayne McCombs <astrothayne@gmail.com>
2017-12-10 20:36:29 -0700

    Fix bash_completion path in deb.

--- ci/before_deploy.bash
+++ ci/before_deploy.bash
@@ -85,7 +85,7 @@ make_deb() {
     install -Dm644 LICENSE-APACHE "$tempdir/usr/share/doc/$PROJECT_NAME/LICENSE-APACHE"
 
     # completions
-    install -Dm644 target/$TARGET/release/build/$PROJECT_NAME-*/out/$PROJECT_NAME.bash "$tempdir/usr/share/bash_completion/completions/$PROJECT_NAME/${PROJECT_NAME}.bash-completion"
+    install -Dm644 target/$TARGET/release/build/$PROJECT_NAME-*/out/$PROJECT_NAME.bash "$tempdir/usr/share/bash-completion/completions/$PROJECT_NAME/${PROJECT_NAME}.bash-completion"
     install -Dm644 target/$TARGET/release/build/$PROJECT_NAME-*/out/$PROJECT_NAME.fish "$tempdir/usr/share/fish/vendor_completions.d/$PROJECT_NAME.fish"
     install -Dm644 target/$TARGET/release/build/$PROJECT_NAME-*/out/_$PROJECT_NAME "$tempdir/usr/share/zsh/site-functions/_$PROJECT_NAME"
 
