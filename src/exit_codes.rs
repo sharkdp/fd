7213f5a88ecf860e0a08d34f8869a1fa6e99a346
fusillicode <fusillicode@gmail.com>
2020-01-25 17:13:47 +0100

    Add collection of job & thread exit codes + default to ExitCode::Error if any ExitCode::Error

--- src/exit_codes.rs
+++ src/exit_codes.rs
@@ -7,9 +7,21 @@ pub enum ExitCode {
 impl Into<i32> for ExitCode {
     fn into(self) -> i32 {
         match self {
-            ExitCode::Success => 0,
-            ExitCode::GeneralError => 1,
-            ExitCode::KilledBySigint => 130,
+            Self::Success => 0,
+            Self::GeneralError => 1,
+            Self::KilledBySigint => 130,
         }
+    }
+}
+
+impl ExitCode {
+    pub fn error_if_any_error(results: Vec<Self>) -> Self {
+        if results.iter().any(|s| match s {
+            Self::GeneralError => true,
+            _ => false,
+        }) {
+            return Self::GeneralError;
+        }
+        Self::Success
     }
 }
