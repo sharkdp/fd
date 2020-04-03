e0cba4b700a8846629d9e6cb91fa032bdd4a249b
sharkdp <davidpeter@web.de>
2020-04-03 12:02:59 +0200

    Remove 'internal' module

--- src/fshelper/mod.rs
+++ src/fshelper/mod.rs
@@ -1,4 +1,6 @@
+use std::borrow::Cow;
 use std::env::current_dir;
+use std::ffi::OsStr;
 use std::fs;
 use std::io;
 #[cfg(any(unix, target_os = "redox"))]
@@ -62,5 +64,21 @@ pub fn is_empty(entry: &walk::DirEntry) -> bool {
         }
     } else {
         false
+    }
+}
+
+#[cfg(any(unix, target_os = "redox"))]
+pub fn osstr_to_bytes(input: &OsStr) -> Cow<[u8]> {
+    use std::os::unix::ffi::OsStrExt;
+    Cow::Borrowed(input.as_bytes())
+}
+
+#[cfg(windows)]
+pub fn osstr_to_bytes(input: &OsStr) -> Cow<[u8]> {
+    let string = input.to_string_lossy();
+
+    match string {
+        Cow::Owned(string) => Cow::Owned(string.into_bytes()),
+        Cow::Borrowed(string) => Cow::Borrowed(string.as_bytes()),
     }
 }
