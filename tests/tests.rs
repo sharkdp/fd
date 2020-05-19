13de5928d13402ac7aaffcc9e16d1fc9c0a8914e
sharkdp <davidpeter@web.de>
2020-05-19 12:08:06 +0200

    Make tests pass

--- tests/tests.rs
+++ tests/tests.rs
@@ -1221,12 +1221,12 @@ fn test_exec_batch() {
 
         te.assert_error(
             &["foo", "--exec-batch", "echo", "{/}", ";", "-x", "echo"],
-            "error: The argument '--exec <cmd>' cannot be used with '--exec-batch <cmd>'",
+            "error: The argument '--exec <cmd>...' cannot be used with '--exec-batch <cmd>...'",
         );
 
         te.assert_error(
             &["foo", "--exec-batch"],
-            "error: The argument '--exec-batch <cmd>' requires a value but none was supplied",
+            "error: The argument '--exec-batch <cmd>...' requires a value but none was supplied",
         );
 
         te.assert_error(
