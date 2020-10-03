d9aba72b068626f0670f690c68156b3a79111001
Matthias Reitinger <reima@edenprime.de>
2020-10-03 19:54:07 +0200

    Add --prune flag

--- src/app.rs
+++ src/app.rs
@@ -216,6 +216,12 @@ pub fn build_app() -> App<'static, 'static> {
                 ),
         )
         .arg(
+            Arg::with_name("prune")
+                .long("prune")
+                .hidden_short_help(true)
+                .long_help("Do not traverse into matching directories.")
+        )
+        .arg(
             Arg::with_name("file-type")
                 .long("type")
                 .short("t")
