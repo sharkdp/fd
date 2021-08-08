2d398dc4a7849d6074b166482548195c187b46f7
David Peter <mail@david-peter.de>
2021-08-08 13:24:56 +0200

    Fix directory-existence check on Windows
    This fixes a bug on Windows where `fd` could not be used on ram disks
    and encrypted folders.
    
    closes #752

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ fn run() -> Result<ExitCode> {
     // Set the current working directory of the process
     if let Some(base_directory) = matches.value_of_os("base-directory") {
         let base_directory = Path::new(base_directory);
-        if !filesystem::is_dir(base_directory) {
+        if !filesystem::is_existing_directory(base_directory) {
             return Err(anyhow!(
                 "The '--base-directory' path '{}' is not a directory.",
                 base_directory.to_string_lossy()
@@ -70,7 +70,7 @@ fn run() -> Result<ExitCode> {
     }
 
     let current_directory = Path::new(".");
-    if !filesystem::is_dir(current_directory) {
+    if !filesystem::is_existing_directory(current_directory) {
         return Err(anyhow!(
             "Could not retrieve current directory (has it been deleted?)."
         ));
@@ -95,7 +95,7 @@ fn run() -> Result<ExitCode> {
         let mut directories = vec![];
         for path in paths {
             let path_buffer = PathBuf::from(path);
-            if filesystem::is_dir(&path_buffer) {
+            if filesystem::is_existing_directory(&path_buffer) {
                 directories.push(path_buffer);
             } else {
                 print_error(format!(
@@ -130,7 +130,7 @@ fn run() -> Result<ExitCode> {
     // Detect if the user accidentally supplied a path instead of a search pattern
     if !matches.is_present("full-path")
         && pattern.contains(std::path::MAIN_SEPARATOR)
-        && filesystem::is_dir(Path::new(pattern))
+        && Path::new(pattern).is_dir()
     {
         return Err(anyhow!(
             "The search pattern '{pattern}' contains a path-separation character ('{sep}') \
