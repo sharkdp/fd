da0d22041fe05af8bbcd11455bd5a40f25a8b5b4
Thayne McCombs <astrothayne@gmail.com>
2021-08-09 01:02:30 -0600

    Address feedback for removing buffering when running on a single thread

--- src/exec/job.rs
+++ src/exec/job.rs
@@ -16,7 +16,7 @@ pub fn job(
     cmd: Arc<CommandTemplate>,
     out_perm: Arc<Mutex<()>>,
     show_filesystem_errors: bool,
-    is_multithread: bool,
+    buffer_output: bool,
 ) -> ExitCode {
     let mut results: Vec<ExitCode> = Vec::new();
     loop {
@@ -35,11 +35,11 @@ pub fn job(
             }
             Err(_) => break,
         };
-        
+
         // Drop the lock so that other threads can read from the receiver.
         drop(lock);
         // Generate a command, execute it and store its exit code.
-        results.push(cmd.generate_and_execute(&value, Arc::clone(&out_perm), is_multithread))
+        results.push(cmd.generate_and_execute(&value, Arc::clone(&out_perm), buffer_output))
     }
     // Returns error in case of any error.
     merge_exitcodes(&results)
@@ -49,7 +49,7 @@ pub fn batch(
     rx: Receiver<WorkerResult>,
     cmd: &CommandTemplate,
     show_filesystem_errors: bool,
-    is_multithread: bool,
+    buffer_output: bool,
 ) -> ExitCode {
     let paths = rx.iter().filter_map(|value| match value {
         WorkerResult::Entry(val) => Some(val),
@@ -60,5 +60,5 @@ pub fn batch(
             None
         }
     });
-    cmd.generate_and_execute_batch(paths, is_multithread)
+    cmd.generate_and_execute_batch(paths, buffer_output)
 }
