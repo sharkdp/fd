73af81bb77ff07b396b90da1bd91ae0755ffd5c7
David Peter <mail@david-peter.de>
2021-11-14 19:06:55 +0100

    Improve --type help text
    closes #823

--- src/app.rs
+++ src/app.rs
@@ -333,14 +333,36 @@ pub fn build_app() -> App<'static, 'static> {
                          empty (e), socket (s), pipe (p)",
                 )
                 .long_help(
-                    "Filter the search by type (multiple allowable filetypes can be specified):\n  \
+                    "Filter the search by type:\n  \
                        'f' or 'file':         regular files\n  \
                        'd' or 'directory':    directories\n  \
                        'l' or 'symlink':      symbolic links\n  \
-                       'x' or 'executable':   executables\n  \
-                       'e' or 'empty':        empty files or directories\n  \
                        's' or 'socket':       socket\n  \
-                       'p' or 'pipe':         named pipe (FIFO)",
+                       'p' or 'pipe':         named pipe (FIFO)\n\n  \
+                       'x' or 'executable':   executables\n  \
+                       'e' or 'empty':        empty files or directories\n\n\
+                     This option can be specified more than once to include multiple file types. \
+                     Searching for '--type file --type symlink' will show both regular files as \
+                     well as symlinks. Note that the 'executable' and 'empty' filters work differently: \
+                     '--type executable' implies '--type file' by default. And '--type empty' searches \
+                     for empty files and directories, unless either '--type file' or '--type directory' \
+                     is specified in addition.\n\n\
+                     Examples:\n  \
+                       - Only search for files:\n      \
+                           fd --type file …\n      \
+                           fd -tf …\n  \
+                       - Find both files and symlinks\n      \
+                           fd --type file --type symlink …\n      \
+                           fd -tf -tl …\n  \
+                       - Find executable files:\n      \
+                           fd --type executable\n      \
+                           fd -tx\n  \
+                       - Find empty files:\n      \
+                           fd --type empty --type file\n      \
+                           fd -te -tf\n  \
+                       - Find empty directories:\n      \
+                           fd --type empty --type directory\n      \
+                           fd -te -td"
                 ),
         )
         .arg(
